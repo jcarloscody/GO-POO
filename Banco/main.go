81d5b619e56f1a44a3b39cc60554e6111e9a39ee
Josué <josuecarlosos2@gmail.com>
2023-06-06 19:23:44 -0300

    new

--- Banco/main.go
+++ Banco/main.go
@@ -13,4 +13,13 @@ func main() {
 	contaCorrente := ContaCorrente{titular: "jouse", numeroAgencia: 2565, saldo: 25.5, numeroConta: 123}
 	fmt.Println(contaCorrente)
 
+	minhaConta := new(ContaCorrente)
+	minhaConta.titular = "outra"
+
+	fmt.Println(*minhaConta)
+
+	contaCorrente2 := ContaCorrente{titular: "jouse", numeroAgencia: 2565, saldo: 25.5, numeroConta: 123}
+
+	fmt.Println(contaCorrente == contaCorrente2)
+
 }
