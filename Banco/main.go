61e431e064c4f69d4cd477cbb9ddda0cb41c0c8d
Josué <josuecarlosos2@gmail.com>
2023-06-06 19:27:35 -0300

    comparacao de endereço de memoria e value

--- Banco/main.go
+++ Banco/main.go
@@ -20,6 +20,12 @@ func main() {
 
 	contaCorrente2 := ContaCorrente{titular: "jouse", numeroAgencia: 2565, saldo: 25.5, numeroConta: 123}
 
-	fmt.Println(contaCorrente == contaCorrente2)
+	fmt.Println(contaCorrente == contaCorrente2) //true, pq compara o valor
+
+	minhaConta2 := new(ContaCorrente)
+	minhaConta2.titular = "outra"
+	fmt.Println(minhaConta2 == minhaConta)   //false, pq compara pelo endereço de memoria
+	fmt.Println(*minhaConta2 == *minhaConta) //true, pq compara o valor
+	fmt.Println(&minhaConta2, &minhaConta)
 
 }
